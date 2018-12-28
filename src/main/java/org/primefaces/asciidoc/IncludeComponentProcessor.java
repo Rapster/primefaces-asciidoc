0405ff8afd6937febbd5e0a728ae0da31b145fb5
Rapster <sebastien.lepage@outlook.com>
2018-12-29 00:14:58 +0100

    Better templating support

--- src/main/java/org/primefaces/asciidoc/IncludeComponentProcessor.java
+++ src/main/java/org/primefaces/asciidoc/IncludeComponentProcessor.java
@@ -1,5 +1,10 @@
 package org.primefaces.asciidoc;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,7 +34,7 @@ public class IncludeComponentProcessor extends PFIncludeProcessor {
     public Object getDataModel(DocumentRuby documentRuby, PreprocessorReader reader, String s, Map<String, Object> config) {
         Matcher matcher = COMPONENT_PATTERN.matcher(s);
         if (matcher.matches()) {
-            return processDataModel(matcher.group(1));
+            return processDataModel(matcher.group(1), documentRuby.getAttributes());
         }
 
         return Collections.emptyMap();
@@ -40,7 +45,7 @@ public class IncludeComponentProcessor extends PFIncludeProcessor {
         return "component.ftl";
     }
 
-    protected Map<String, Object> processDataModel(String tagName) {
+    protected Map<String, Object> processDataModel(String tagName, Map<String, Object> config) {
         Tag tag =  PFAsciiDoc.INSTANCE.findTag(tagName);
         if (tag == null) {
             throw new IllegalArgumentException("Tag name not found: " + tagName);
@@ -60,6 +65,24 @@ public class IncludeComponentProcessor extends PFIncludeProcessor {
             map.put("behavior", tag.getBehavior());
         }
 
+        String content = readADOCComponentFile(tagName, config);
+        map.put("content", content);
         return map;
+    }
+
+    protected String readADOCComponentFile(String tagName, Map<String, Object> config) {
+        String pathDoc = (String) config.get("components");
+        if (pathDoc == null) {
+            return null;
+        }
+
+        Path pathFile = Paths.get(pathDoc, "_" + tagName + ".adoc");
+        try {
+            byte[] bytes = Files.readAllBytes(pathFile);
+            return new String(bytes, StandardCharsets.UTF_8);
+        }
+        catch (IOException e) {
+            throw new IllegalArgumentException("Can't read " + pathDoc, e);
+        }
     }
 }
